ddde19c6b502930d7f23b447bfcd689352dafd00
Soundarya Baskar <baskarsandra@gmail.com>
2022-08-11 18:42:58 -0500

    update with java files =

---
+++ app/src/main/java/com/example/kidsapp/exploretips.java
@@ -0,0 +1,14 @@
+package com.example.kidsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class exploretips extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exploretips);
+    }
+}
