9c9eb0e07776387ed197d9f8d961fb1efd0bf1b7
Shiv Patel <56481777+spatel1245@users.noreply.github.com>
2022-08-11 19:27:33 -0500

    Add buttons for savingstree page

--- app/src/main/java/com/example/kidsapp/MainActivity.java
+++ app/src/main/java/com/example/kidsapp/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.kidsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homescreen);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, savingstree.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kidsapp/savingstree.java
+++ app/src/main/java/com/example/kidsapp/savingstree.java
@@ -2,13 +2,33 @@ package com.example.kidsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class savingstree extends AppCompatActivity {
+    private Button toSavingsTree;
+    private Button toCreateNewGoal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_savingstree);
+
+        toSavingsTree = (Button)findViewById(R.id.addToSavingsTree);
+        toCreateNewGoal = (Button)findViewById(R.id.createGoal);
+
+        toSavingsTree.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(savingstree.this, addtosavingsaccount.class));
+            }
+        });
+
+        toCreateNewGoal.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(savingstree.this, creategoal.class));
+            }
+        });
     }
 }
